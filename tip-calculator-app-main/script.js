dfd8666031d68dc584bc483a793c3e1f4cb7da23
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-03-04 16:03:53 -0300

    calculadora pronta

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,3 +1,16 @@
-function calc(){
-    document.alert('teste')
+function calc(ntip){
+    var fnbill = document.getElementById('billvalue')
+    var fnpeople = document.getElementById('npeople')
+
+    var nbill = Number(fnbill.value)
+    var npeople = Number(fnpeople.value)
+
+    var tip = ((nbill*ntip)/100)/npeople
+    tip = Number(tip.toFixed(2))
+
+    var totaltip = ((tip*npeople)+nbill)/npeople
+    totaltip = Number(totaltip.toFixed(2))
+
+    window.alert(`tip is ${tip} and total is ${totaltip}`)
+
 }
