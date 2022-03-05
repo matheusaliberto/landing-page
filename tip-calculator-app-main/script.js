35016082d41f92d4369b7e53b816aad9481123ef
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-03-05 15:33:38 -0300

    preparação para adapt display

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,25 +1,36 @@
 function calc(ntip){
-    var fnbill = document.getElementById('billvalue')
-    var fnpeople = document.getElementById('npeople')
+    if(ntip == 88.838){
+        ntip = window.prompt("Write a custom value in %: ")
+        calc(ntip)
+    }
+    else{
+        if(ntip == 88.839){
+            var nbill = 0.00
+            var npeople = 0.00
+            var tip = 0.00
+            var totaltip = 0.00
+        }
+        else{
+            var fnbill = document.getElementById('billvalue')
+            var fnpeople = document.getElementById('npeople')
 
-    var nbill = Number(fnbill.value)
-    var npeople = Number(fnpeople.value)
+            var nbill = Number(fnbill.value)
+            var npeople = Number(fnpeople.value)
 
-    var tip = ((nbill*ntip)/100)/npeople
-    tip = Number(tip.toFixed(2))
+            var tip = ((nbill*ntip)/100)/npeople
+            tip = Number(tip.toFixed(2))
 
-    var totaltip = ((tip*npeople)+nbill)/npeople
-    totaltip = Number(totaltip.toFixed(2))
+            var totaltip = ((tip*npeople)+nbill)/npeople
+            totaltip = Number(totaltip.toFixed(2))
+        }
 
-    var result = document.getElementById('resulttip')
-    var total = document.getElementById('totaltip')
+        var result = document.getElementById('resulttip')
+        var total = document.getElementById('totaltip')
 
-    value1 = `$${tip}`
-    value2 = `$${totaltip}`
+        value1 = `$${tip}`
+        value2 = `$${totaltip}`
 
-    result.innerText = `${value1}`
-    total.innerText = `${value2}`
-
-    
-
+        result.innerText = `${value1}`
+        total.innerText = `${value2}`
+    }
 }
